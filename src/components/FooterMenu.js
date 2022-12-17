b09014d44eba3f7598b9e87f35f61e9bf30bf4cd
Arthur Saito <arthur_saito1998@hotmail.com>
2022-12-17 17:23:15 -0300

    v 3.1 Added FooterMenuBar

---
+++ src/components/FooterMenu.js
@@ -0,0 +1,55 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const FooterMenu = () =>{
+    return(
+        <FooterContainer>
+            <Link to="/habits">
+                <UL>Habitos</UL>
+            </Link>
+            <Link to="/today">
+                 <CircleBar type="button" value="Hoje"></CircleBar>
+            </Link>
+            <Link to="/history">
+                <UL>Historico</UL>
+            </Link>
+        </FooterContainer>
+    );
+}
+
+const FooterContainer=styled.footer`
+    width: 100%;
+    height: 70px;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background: #FFFFFF;
+    box-sizing: border-box;
+    padding: 0px 30px;
+`
+
+const UL = styled.p`
+    font-family: "Lexend Deca", sans-serif;
+    font-weight: 400px;
+    font-size: 18px;
+    line-height: 22px;
+    color: #52B6FF;
+`
+
+const CircleBar = styled.input`
+    width: 90px;
+    height: 90px;
+    border-radius: 50%;
+    background-color: #52B6FF;
+    font-family: "Lexend Deca", sans-serif;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 22px;
+    color: #FFFFFF;
+    margin-bottom: 35px;
+`
+
+export default FooterMenu
--- src/components/pages/Today/Today.js
+++ src/components/pages/Today/Today.js
@@ -1,19 +1,43 @@
 import axios from "axios";
 import styled from "styled-components";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import Context from "../../Context";
 import Header from "../../Header";
+import FooterMenu from "../../FooterMenu";
 
 const Today = () =>{
+    const WEEK = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado"];
+    const date = new Date();
+    const day = date.getDay();
 
     return(
         <TodayContainer>
             <Header></Header>
+            <FooterMenu/>
+            <h2>{WEEK[day]}, {date.toLocaleDateString()}</h2>
         </TodayContainer>
+        
     )
 }
 
 const TodayContainer=styled.main`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    background-color: #E5E5E5;
+    margin:95px 0px 110px 0px;
+    h2{
+        width: 80%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-family: "Lexend Deca", sans-serif;
+        font-weight: 400;
+        font-size: 23px;
+        line-height: 29px;
+        color: #126BA5;
+    }
 `
 
 export default Today;
